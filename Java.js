4bd9553545a282d6c46f17ea90b4a063a97e3656
huniiali1122@gmail.com <huniiali1122@gmail.com>
2025-12-19 00:06:34 +0500

    updated

--- Java.js
+++ Java.js
@@ -1,3 +1,17 @@
-console.log( "hey hunain how are u")
+// console.log( "hey hunain how are u")
 
 // alert("hey")
+
+
+// var
+// var hunain1 = "hunain kese "
+// console.log(hunain1)
+
+// let
+
+// let hunain1 = "20"
+// console.log(hunain1)
+
+
+let hunain = "30"
+console.log(typeof(hunain))
