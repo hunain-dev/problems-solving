e2f1518fffd3782fb4b7cdb491e062fd7c14a5d2
huniiali1122@gmail.com <huniiali1122@gmail.com>
2025-12-19 15:59:19 +0500

    updated

--- Java.js
+++ Java.js
@@ -236,25 +236,96 @@
 // console.log("love")
 
 
-const apicalling = document.getElementById("user");
+// const apicalling = document.getElementById("user");
 
-async function callinglist() {
-    try {
-        const data = await fetch("https://jsonplaceholder.typicode.com/users")
-        const res = await data.json()
-        console.log(res) 
+// async function callinglist() {
+//     try {
+//         const data = await fetch("https://jsonplaceholder.typicode.com/users")
+//         const res = await data.json()
+//         console.log(res) 
 
-        apicalling.innerHTML = res.map(user => {
-            return `<p>${user.id} ${user.name} ${user.email}</p>`;
-        });
+//         apicalling.innerHTML = res.map(user => {
+//             return `<p>${user.id} ${user.name} ${user.email}</p>`;
+//         });
 
-    } catch {
-        console.log("error found")
+//     } catch {
+//         console.log("error found")
         
-    }
+//     }
 
     
-}
+// }
 
-callinglist()
+// callinglist()
 
+
+// function closure() {
+//     let count = 0;
+
+
+//    return function() {
+//         count++
+//         console.log(count)
+        
+//     }
+// }
+// const coutning = closure()
+// coutning();
+// coutning();
+// coutning();
+// coutning();
+// coutning();
+
+
+const numbers = [1, 2, 3, 4, 5, 6,"hunain"];
+
+// const arr = numbers.map(function(square){
+//     return square * 2
+    
+// })
+
+// console.log(arr)
+
+
+// const arr = numbers.filter((function(fitleing){
+//    return fitleing % 2 === 0
+// }))
+// console.log(arr)
+
+// const sum  = numbers.reduce(function(suming,acc){
+//     return suming + acc
+    
+// }, 0)
+// console.log(sum)
+
+
+// async function fetchingapi() {
+//     try {
+//         const response = await fetch("https://jsonplaceholder.typicode.com/users")
+//         const data = await response.json();
+//         console.log(data)
+//         console.log(data.name)
+//         data.forEach(user => {
+//             console.log(user.name)
+            
+//         });
+//     } catch {
+//         console.log("errror found")
+        
+//     }
+
+    
+// }fetchingapi()
+
+
+// const user = {
+//     name: "Hunain",
+//     sub:"khan",
+//     getName() {
+//         return this.user;
+//       }
+      
+//   };
+
+//   console.log(user.getName());
+  
